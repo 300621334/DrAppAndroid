29172a965358ed6daf00e099fd2af396efcf1996
Shafiq <shafiqu@my.centennialcollege.ca>
2018-05-17 00:41:17 -0400

    Zooms to Bounds

--- app/src/main/java/comp231/drbooking/Login.java
+++ app/src/main/java/comp231/drbooking/Login.java
@@ -1,34 +1,48 @@
 package comp231.drbooking;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceFilter;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.util.Map;
+
+import static com.google.android.gms.location.places.Place.TYPE_DOCTOR;
+import static com.google.android.gms.location.places.Place.TYPE_HOSPITAL;
 
 public class Login extends AppCompatActivity {
 
     //region >>> Variables
-    boolean isFaculty = false , isFacultyRadChecked = true;
+    boolean isFaculty = false, isFacultyRadChecked = true;
     RadioGroup radGp;
     RadioButton radChecked;
     EditText uNameView, uPassView;
     boolean hasAccount = false;
     SharedPreferences prefs;
     Intent intent;
-    Map<String,?> allPrefs;
-    int PLACE_PICKER_REQUEST  = 1, numOfPrefs , uName_uPass_pairs;// counter = 1;
+    Map<String, ?> allPrefs;
+    int PLACE_PICKER_REQUEST = 1, numOfPrefs, uName_uPass_pairs;// counter = 1;
     String uName, uPass, uNameEntered, uPassEntered;
+    double longitude,latitude;
     //endregion
 
     @Override
@@ -37,22 +51,67 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login or Create New Account");
         //get references
-        uNameView = (EditText)findViewById(R.id.txtLoginName);
-        uPassView = (EditText)findViewById(R.id.txtLoginPass);
+        uNameView = (EditText) findViewById(R.id.txtLoginName);
+        uPassView = (EditText) findViewById(R.id.txtLoginPass);
 
+        ////if no permission then assign some default location like downtown or Centennial college
+        //String dummyForBreakPoint = "";
+    }
+
+
+
+    private void getCurrentLoc() //grant permissions for "Location" from phone/emulator
+    {
+        //https://stackoverflow.com/questions/2227292/how-to-get-latitude-and-longitude-of-the-mobile-device-in-android
+        //More detailed Criteris : https://stackoverflow.com/questions/2699215/get-the-current-location-gps-wifi
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ////if no permission then assign some default location like downtown or Centennial college
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //location obj is NULL in newly installed app bcoz no "Last Known Loc" there. So turn Google Maps inbuilt app & click target icon to get at least one loc in memory
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        Toast.makeText(this, longitude +"---"+ latitude, Toast.LENGTH_LONG).show();
     }
 
     //Login btn clk
     public void clk_Login(View view)
     {
+        getCurrentLoc();
+
+        //top-left & bottom-right corners of what PlacePicker will display
+        //use this site to get coords : https://www.latlong.net
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(new LatLng(43.790272, -79.234127))
+                .include(new LatLng(43.759768, -79.225224))
+                .build();
+
+        //filter places e.g. clinics etc
+
+        PlaceFilter placeFilter = new PlaceFilter();
+        placeFilter.equals(TYPE_DOCTOR | TYPE_HOSPITAL);//int Place_IDs permitted : https://developers.google.com/android/reference/com/google/android/gms/location/places/Place
+
         //go to PlacePicker : https://www.youtube.com/watch?v=Rh9x90lqPHc
         PlacePicker.IntentBuilder iBuilder = new PlacePicker.IntentBuilder(); //compile 'com.google.android.gms:play-services-places:9.2.0'
-
+        //iBuilder.setLatLngBounds(MapUtils.getLatLngBounds(new LatLng((double) latitude, (double) longitude)));
         Intent i;
 
         try
         {
-            i = iBuilder.build(Login.this);
+            i = iBuilder
+                    .setLatLngBounds(bounds)
+                    .build(Login.this);
+
+
             startActivityForResult(i, PLACE_PICKER_REQUEST);
         }
         catch (GooglePlayServicesRepairableException e)
